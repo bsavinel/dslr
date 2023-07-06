b2ddf09da6c15fc400c7f0472a5791923acb6db8
Benoit Savinel <bsavinel@student.42.fr>
2023-07-06 12:29:14 +0200

    STYLE: better code

--- histogram.py
+++ histogram.py
@@ -4,6 +4,8 @@ import seaborn as sns
 
 df = pd.read_csv('datasets/dataset_train.csv')
 
+house = ["Ravenclaw", "Slytherin", "Gryffindor", "Hufflepuff"]
+
 df_Ravenclaw = df[df['Hogwarts House'] == "Ravenclaw"].select_dtypes(['number'])
 df_Slytherin = df[df['Hogwarts House'] == "Slytherin"].select_dtypes(['number'])
 df_Gryffindor = df[df['Hogwarts House'] == "Gryffindor"].select_dtypes(['number'])
@@ -11,9 +13,9 @@ df_Hufflepuff = df[df['Hogwarts House'] == "Hufflepuff"].select_dtypes(['number'
 
 fig, axs = plt.subplots(ncols=3, nrows=int((len(df_Ravenclaw.columns) - 1) / 3 + 1), figsize=(20, 10))
 fig.tight_layout(pad=2)
-j = 0
-for i in range(1, len(df_Ravenclaw.columns)):
-	print((i - 1) % 3, int((i - 1) / 3))
-	sns.barplot(x=["Ravenclaw", "Slytherin", "Gryffindor", "Hufflepuff"], y=[df_Ravenclaw.sum(axis=0)[i], df_Slytherin.sum(axis=0)[i], df_Gryffindor.sum(axis=0)[i], df_Hufflepuff.sum(axis=0)[i]], ax=axs[int((i - 1) / 3), (i - 1) % 3]).set(title=df_Ravenclaw.columns[i])
 
+for i in range(1, len(df_Ravenclaw.columns)):
+	col = (i - 1) % 3
+	row = int((i - 1) / 3)
+	sns.barplot(x=house, y=[df_Ravenclaw.sum(axis=0)[i], df_Slytherin.sum(axis=0)[i], df_Gryffindor.sum(axis=0)[i], df_Hufflepuff.sum(axis=0)[i]], ax=axs[row, col]).set(title=df_Ravenclaw.columns[i])
 plt.show()
